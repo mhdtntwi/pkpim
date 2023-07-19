560c397d1a30802c2dce8ed7cd7acf7fd0097d17
mhdtntwi <offcltntwi@gmail.com>
2023-07-20 00:26:50 +0800

    generate logo

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/images/logo.png',
             ],
             refresh: true,
         }),
